eb6c2a31a1189c6d9ef8fd40714a05f38299a952
suzan <suzan_aziz2000@yahoo.com>
2011-12-04 21:08:36 +0200

    json android

--- src/com/cobone/CoboneActivity.java
+++ src/com/cobone/CoboneActivity.java
@@ -2,71 +2,77 @@ package com.cobone;
 
 import android.app.Activity;
 import android.os.Bundle;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import java.util.ArrayList;
+import java.util.HashMap;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import com.cobone.JSONfunctions;
 
-import android.app.Activity;
+
+
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
-public class CoboneActivity extends Activity {
+public class CoboneActivity extends ListActivity {
     /** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-//		String readTwitterFeed = readTwitterFeed();
-//		try {
-//			JSONArray jsonArray = new JSONArray(readTwitterFeed);
-//			Log.i("LEE",
-//					"first Number of entries " + jsonArray.length());
-//			for (int i = 0; i < jsonArray.length(); i++) {
-//				JSONObject jsonObject = jsonArray.getJSONObject(i);
-//				Log.i("LEE", jsonObject.getString("text"));
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-	}
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.deallist);
+	        
+	        ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
+	      
+	        JSONObject json = JSONfunctions.getJSONfromURL("http://192.168.1.2:8080/dealAPP/searchdeals.htm");
 
-//	public String readTwitterFeed() {
-//		StringBuilder builder = new StringBuilder();
-//		HttpClient client = new DefaultHttpClient();
-//		HttpGet httpGet = new HttpGet(
-//				"http://twitter.com/statuses/user_timeline/vogella.json");
-//		try {
-//			HttpResponse response = client.execute(httpGet);
-//			StatusLine statusLine = response.getStatusLine();
-//			int statusCode = statusLine.getStatusCode();
-//			if (statusCode == 200) {
-//				HttpEntity entity = response.getEntity();
-//				InputStream content = entity.getContent();
-//				BufferedReader reader = new BufferedReader(
-//						new InputStreamReader(content));
-//				String line;
-//				while ((line = reader.readLine()) != null) {
-//					builder.append(line);
-//				}
-//			} else {
-//				Log.e("LEE", "Failed to download file");
-//			}
-//		} catch (ClientProtocolException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		return builder.toString();
-//	}
+//	        JSONObject json = JSONfunctions.getJSONfromURL("http://api.geonames.org/earthquakesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&username=demo");
+	                
+	        try{
+	        	
+//	        	JSONArray  earthquakes = json.getJSONArray("earthquakes");
+	        	JSONArray  earthquakes = json.getJSONArray("list");
+
+		        for(int i=0;i<earthquakes.length();i++){						
+					HashMap<String, String> map = new HashMap<String, String>();	
+					JSONObject e = earthquakes.getJSONObject(i);
+					
+					map.put("id",  e.getString("id"));
+		        	map.put("title", "dealTitle:" + e.getString("title"));
+		        	map.put("titleDescription", "titleDescription: " +  e.getString("description"));
+		        	mylist.add(map);			
+				}		
+	        }catch(JSONException e)        {
+	        	 Log.e("log_tag", "Error parsing data "+e.toString());
+	        }
+	        
+	        
+	        ListAdapter adapter = new SimpleAdapter(this, mylist , R.layout.dealbrief, 
+	                        new String[] { "title", "titleDescription" }, 
+	                        new int[] { R.id.item_title, R.id.item_subtitle });
+	        
+	        setListAdapter(adapter);
+	        
+	        final ListView lv = getListView();
+	        lv.setTextFilterEnabled(true);	
+	        lv.setOnItemClickListener(new OnItemClickListener() {
+	        	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {        		
+	        		@SuppressWarnings("unchecked")
+					HashMap<String, String> o = (HashMap<String, String>) lv.getItemAtPosition(position);	        		
+	        		Toast.makeText(CoboneActivity.this, "ID '" + o.get("id") + "' was clicked.", Toast.LENGTH_SHORT).show(); 
+
+				}
+			});
+	    }
 }

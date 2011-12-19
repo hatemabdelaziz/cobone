7c5f9cbb53982cf99968fb3f43c94a2ac1455e8d
hatem <ha_lort@hotmail.com>
2011-12-20 00:43:53 +0200

    image lazy loading

--- src/com/cobone/AndroidList.java
+++ src/com/cobone/AndroidList.java
@@ -3,9 +3,11 @@ package com.cobone;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
@@ -24,17 +26,25 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cobone.library.ImageLoader;
+import com.cobone.library.LazyAdapter;
+
 public class AndroidList extends ListActivity {
 	SharedPreferences sharedPreferences;
+	String[] items;
+    LazyAdapter adapter;
 	public class MyCustomAdapter extends ArrayAdapter<HashMap<String, Object>> {
 		private final List<HashMap<String, Object>> list;
 		private final Activity context;
-		
+	    private String[] data;
+	    public ImageLoader imageLoader; 
 		public MyCustomAdapter(Activity  context, int textViewResourceId,
-				List<HashMap<String, Object>> list) {
+				List<HashMap<String, Object>> list , String[] images) {
 			super(context, textViewResourceId, list);
 			this.context = context;
 			this.list = list;
+			data=images;
+	        imageLoader=new ImageLoader(context.getApplicationContext());
 			// TODO Auto-generated constructor stub
 		}
 
@@ -57,13 +67,48 @@ public class AndroidList extends ListActivity {
 			ImageView imgView=(ImageView)view.findViewById(R.id.imageView1);
 //	         Drawable drawable = JSONfunctions.LoadImageFromWebOperations(list.get(position).get("photoUrl"));
 	         imgView.setImageDrawable((Drawable) list.get(position).get("photoUrl"));
-			
+	         ImageView image=(ImageView)view.findViewById(R.id.imageView1);
+	         //text.setText("item "+position);
+	         imageLoader.DisplayImage(data[position], image);
 			return view;
 		}
 	}
 //	String[] DayOfWeek = {"Sunday", "Monday", "Tuesday",
 //			"Wednesday", "Thursday", "Friday", "Saturday"
 //	};
+	
+	
+	
+	
+//class AddStringTask extends AsyncTask<Void, String, Void> {
+//  @Override
+//  protected Void doInBackground(Void... unused) {
+//    for (String item : items) {
+//      publishProgress(item);
+//      SystemClock.sleep(200);
+//    }
+//    
+//    return(null);
+//  }
+//  
+//  @SuppressWarnings("unchecked")
+//  @Override
+//  protected void onProgressUpdate(String... item) {
+//	Drawable drawable = JSONfunctions.LoadImageFromWebOperations(item[0]);
+//	List<HashMap<String, Object>> mylist = new ArrayList<HashMap<String, Object>>();				
+//	HashMap<String, Object> map = new HashMap<String, Object>();
+// 	map.put("photoUrl",drawable);
+// 	mylist.add(map);
+// 	((ArrayAdapter<String>)getListAdapter()).;
+//  }
+//  
+//  @Override
+//  protected void onPostExecute(Void unused) {
+//    Toast
+//      .makeText(AndroidList.this, "Done!", Toast.LENGTH_SHORT)
+//      .show();
+//  }
+//}
 	
     /** Called when the activity is first created. */
     @Override
@@ -81,6 +126,7 @@ public class AndroidList extends ListActivity {
         try{     	
         	JSONArray  earthquakes = json.getJSONArray("list"); 
         	Toast.makeText(AndroidList.this, "deals size"+earthquakes.length(), Toast.LENGTH_LONG).show();
+        	items = new String[earthquakes.length()];
         	for(int i=0;i<earthquakes.length();i++){						
 				HashMap<String, Object> map = new HashMap<String, Object>();	
 				JSONObject e = earthquakes.getJSONObject(i);
@@ -92,19 +138,24 @@ public class AndroidList extends ListActivity {
 	        	map.put("end",e.getString("end"));
 	        	map.put("price",dealPrice+" "+ dealCurrency+" ("+dealValue+" "+ dealCurrency+")");       	
 //	        	map.put("photoUrl",e.getString("photo"));
-	        	  Drawable drawable = JSONfunctions.LoadImageFromWebOperations(e.getString("photo"));
-	        	  map.put("photoUrl",drawable);
+	        	  //Drawable drawable = JSONfunctions.LoadImageFromWebOperations(e.getString("photo"));
+	        	 // map.put("photoUrl",drawable);
+	        	items[i] = e.getString("photo");
 	        	  mylist.add(map);
         	}		
+        	//list=(ListView)findViewById(R.id.list);
+            
+//        	new AddStringTask().execute();
         }catch(JSONException e)        {
         	 Log.e("log_tag", "Error parsing data "+e.toString());
         }
 //      requestWindowFeature(Window.FEATURE_PROGRESS);
          
        
-        setListAdapter(new MyCustomAdapter(AndroidList.this, R.layout.dealbrief, mylist));
+        setListAdapter(new MyCustomAdapter(AndroidList.this, R.layout.dealbrief, mylist ,items));
         final ListView lv = getListView();
         lv.setTextFilterEnabled(true);	
+        lv.setAdapter(adapter);
         lv.setOnItemClickListener(new OnItemClickListener() {
         	public void onItemClick(AdapterView<?> parent, View view, int position, long id) { 
 //        		Toast.makeText(AndroidList.this, "we get deal to you", Toast.LENGTH_SHORT).show(); 
